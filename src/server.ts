ac0ca211c775b40c251d73c87402515c58955498
Rajveer Srivastava <irajveersrivastava@gmail.com>
2024-05-23 16:41:40 +0530

    Project Setup

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import express, { Request, Response } from "express";
+import connectToDatabase from "./db";
+
+const application = express();
+
+application.use(express.json());
+
+const PORT = 1337;
+connectToDatabase();
+
+application.get("/ping", (request: Request, response: Response) => {
+  response.send("Pong");
+});
+
+application.listen(PORT, () => {
+  console.log("Server up and running");
+});
