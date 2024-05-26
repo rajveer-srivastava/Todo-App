a25f62138a5a085836db5b4790f40279910136a6
Rajveer Srivastava <irajveersrivastava@gmail.com>
2024-05-26 19:26:31 +0530

    User Login API

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,8 +1,9 @@
 import express from "express";
-import { createUser } from "../controllers/user.controller";
+import { createUser, loginUser } from "../controllers/user.controller";
 
 const userRoutes = express.Router();
 
 userRoutes.route("/create").post(createUser);
+userRoutes.route("/login").post(loginUser);
 
 export default userRoutes;
